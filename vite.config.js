80bb919baa8538303c5a0fe476711d97d915963b
raushan736834 <raushan736834@gmail.com>
2025-08-07 01:06:02 +0530

    config change

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,8 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
-const isProd = process.env.NODE_ENV === 'production'
-
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: isProd ? '/Doctor-Appointment/' : '/',
+  base: '/',
   plugins: [react()],
   server: {
     watch: {
